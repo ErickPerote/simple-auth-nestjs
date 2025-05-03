64094cfc20edd2afab8c033b6e3baf82ce449450
ErickPerote <erickperote.25@gmail.com>
2025-05-03 19:37:15 -0300

    test-2

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,126 +1,139 @@
-import { ConflictException, HttpException, HttpStatus, Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { User } from "src/entity/user.entity";
-import { Repository } from "typeorm";
+import {
+  ConflictException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+  Abstract,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entity/user.entity';
+import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
-import { UpdateUserDto, UserDto } from "src/modules/user/user.dto";
-
+import { UpdateUserDto, UserDto } from 'src/modules/user/user.dto';
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectRepository(User)
-        private userRepository: Repository<User>
-    ) { }
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
 
-    findByUsername(username: string): Promise<User> {
-        const userName = this.userRepository.findOneBy({ username });
+  findByUsername(username: string): Promise<User> {
+    const userName = this.userRepository.findOneBy({ username });
 
-        if (!userName) {
-            throw new NotFoundException('user not found')
-        }
-        return userName
+    if (!userName) {
+      throw new NotFoundException('user not found');
+    }
+    return userName;
+  }
+
+  async findByEmail(email: string) {
+    const user = await this.userRepository.findOneBy({ email });
+
+    if (!user) {
+      throw new NotFoundException('User not found!');
     }
 
-    async findByEmail(email: string) {
-        const user = await this.userRepository.findOneBy({ email })
+    return user;
+  }
 
-        if (!user) {
-            throw new NotFoundException('User not found!')
-        }
+  findById(userId: string): Promise<User> {
+    const user = this.userRepository.findOneBy({ id: userId });
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return user;
+  }
 
-        return user
+  async create(body: UserDto, password: string): Promise<User> {
+    const existingUser = await this.userRepository.findOneBy({
+      id: body.username,
+    });
+
+    if (existingUser) {
+      throw new ConflictException('User already exist!');
     }
 
-    findById(userId: string): Promise<User> {
-        const user = this.userRepository.findOneBy({ id: userId });
-        if (!user) {
-            throw new NotFoundException('user not found')
-        }
-        return user
+    const hash = await bcrypt.hash(password, 10);
+
+    const user = new User();
+    user.username = body.username;
+    user.password = hash;
+    user.email = body.email;
+    user.roles = body.roles;
+
+    try {
+      return await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === 'ER_DUP_ENTRY') {
+        throw new ConflictException('User already exist!');
+      }
+      throw new HttpException(
+        'Unknow error in register user',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async validateUser(email: string): Promise<User> {
+    const user = await this.userRepository
+      .createQueryBuilder('user')
+      .addSelect(['user.password', 'user.roles'])
+      .where('user.email = :email', { email })
+      .getOne();
+
+    if (!user) {
+      throw new NotFoundException('user not exist!');
+    }
+    return user;
+  }
+
+  async registerEmailOtpCode(userId: string, code: number) {
+    const user = await this.findById(userId);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
     }
 
-    async create(body: UserDto, password: string): Promise<User> {
-        const existingUser = await this.userRepository.findOneBy({ id: body.username });
+    const expiresAt = new Date(Date.now() + 10 * 60 * 1000);
 
-        if (existingUser) {
-            throw new ConflictException('User already exist!');
-        }
+    user.otpEmailCode = code;
+    user.otpCodeEmailExpiresAt = expiresAt;
 
-        const hash = await bcrypt.hash(password, 10);
+    await this.userRepository.save(user);
 
-        const user = new User();
-        user.username = body.username;
-        user.password = hash;
-        user.email = body.email
-        user.roles = body.roles;
+    return user;
+  }
 
-        try {
-            return await this.userRepository.save(user);
-        } catch (error) {
-            if (error.code === 'ER_DUP_ENTRY') {
-                throw new ConflictException('User already exist!');
-            }
-            throw new HttpException('Unknow error in register user', HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+  async resetPassword(userId: string, password: string) {
+    const user = await this.findById(userId);
+
+    const hash = await bcrypt.hash(password, 10);
+
+    user.password = hash;
+
+    return await this.userRepository.save(user);
+  }
+
+  // user.service.ts
+  async update(userId: string, updateData: UpdateUserDto): Promise<User> {
+    const user = await this.findById(userId);
+
+    if (updateData.username) user.username = updateData.username;
+    if (updateData.email) user.email = updateData.email;
+    if (updateData.roles) user.roles = updateData.roles;
+
+    try {
+      return await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === 'ER_DUP_ENTRY') {
+        throw new ConflictException('Email or username already in use');
+      }
+      throw new HttpException(
+        'Failed to update user',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
-
-    async validateUser(email: string): Promise<User> {
-        const user = await this.userRepository
-            .createQueryBuilder('user')
-            .addSelect(['user.password', 'user.roles'])
-            .where('user.email = :email', { email })
-            .getOne();
-
-        if (!user) {
-            throw new NotFoundException('user not exist!')
-        }
-        return user
-    }
-
-    async registerEmailOtpCode(userId: string, code: number) {
-        const user = await this.findById(userId);
-
-        if (!user) {
-            throw new NotFoundException('User not found');
-        }
-
-        const expiresAt = new Date(Date.now() + 10 * 60 * 1000);
-
-        user.otpEmailCode = code;
-        user.otpCodeEmailExpiresAt = expiresAt;
-
-        await this.userRepository.save(user);
-
-        return user;
-    }
-
-    async resetPassword(userId: string, password: string) {
-        const user = await this.findById(userId);
-
-        const hash = await bcrypt.hash(password, 10)
-
-        user.password = hash
-
-        return await this.userRepository.save(user)
-    }
-
-    // user.service.ts
-    async update(userId: string, updateData: UpdateUserDto): Promise<User> {
-        const user = await this.findById(userId);
-
-        if (updateData.username) user.username = updateData.username;
-        if (updateData.email) user.email = updateData.email;
-        if (updateData.roles) user.roles = updateData.roles;
-
-        try {
-            return await this.userRepository.save(user);
-        } catch (error) {
-            if (error.code === 'ER_DUP_ENTRY') {
-                throw new ConflictException('Email or username already in use');
-            }
-            throw new HttpException('Failed to update user', HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
+  }
 }
