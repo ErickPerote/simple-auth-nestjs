497af8b720fe7e2c5b7f7a93fa126500a151b90f
ErickPerote <erickperote.25@gmail.com>
2025-08-26 21:44:35 -0300

    add pre-commit husky and adjusts in auth

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,51 +1,63 @@
-import { BadRequestException, Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Request, UseGuards } from "@nestjs/common";
-import { Public } from "src/config/publicRoutes.decorator";
-import { AuthGuard } from "src/guard/auth.guard";
-import { AuthDto } from "src/modules/auth/auth.dto";
-import { AuthService } from "src/modules/auth/auth.service";
-import { UserService } from "src/modules/user/user.service";
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Request,
+} from '@nestjs/common';
+import { Public } from 'src/config/publicRoutes.decorator';
+import { AuthDto } from 'src/modules/auth/auth.dto';
+import { AuthService } from 'src/modules/auth/auth.service';
+import { UserService } from 'src/modules/user/user.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(
-        private authService: AuthService,
-        private userService: UserService,
-    ) { }
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
 
-    @Public()
-    @HttpCode(HttpStatus.OK)
-    @Post('login')
-    async login(@Body() body: AuthDto) {
-        const user = await this.userService.validateUser(body.email);
-        
-        return this.authService.login(user, body.password);
+  @Public()
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  async login(@Body() body: AuthDto) {
+    const user = await this.userService.validateUser(body.email);
+
+    return this.authService.login(user, body.password);
+  }
+
+  @Get('whoame')
+  async getProfile(@Request() req) {
+    return await this.userService.findById(req.user.id);
+  }
+
+  //TODO: criar método que valida email ao invés de usar tipo primitivo(string)
+  @Public()
+  @Post('forgot-password-code')
+  async forgotPasswordCode(@Body('email') email: string) {
+    const data = await this.authService.generateOtpCode(email);
+
+    return data;
+  }
+
+  @Public()
+  @Post('reset-password/:userId')
+  async resetPassword(
+    @Param('userId') userId: string,
+    @Body('password') password: string,
+    @Body('code') code: number,
+  ) {
+    const isValidCode = await this.authService.verifyOtpEmailCode(userId, code);
+
+    if (!isValidCode) {
+      throw new Error('sa');
     }
 
-    @Get('whoame')
-    async getProfile(@Request() req) {
-        return await this.userService.findById(req.user.id);
-    }
+    const newPass = await this.userService.resetPassword(userId, password);
 
-    //TODO: criar método que valida email ao invés de usar tipo primitivo(string)
-    @Public()
-    @Post('forgot-password-code')
-    async forgotPasswordCode(@Body('email') email: string) {
-        const data = await this.authService.generateOtpCode(email)
-
-        return data 
-    }
-
-    @Public()
-    @Post('reset-password/:userId')
-    async resetPassword(@Param('userId') userId: string, @Body('password') password: string, @Body('code') code: number) {
-        const isValidCode = await this.authService.verifyOtpEmailCode(userId, code);
-
-        if(!isValidCode) {
-            throw new Error('sa')
-        }
-
-        const newPass = await this.userService.resetPassword(userId, password);
-
-        return newPass
-    }
-} 
+    return newPass;
+  }
+}
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,12 +1,20 @@
-import { Body, Controller, HttpCode, HttpStatus, Param, Patch, Post, UseFilters } from "@nestjs/common";
-import { Public } from "src/config/publicRoutes.decorator";
-import { User } from "src/entity/user.entity";
-import { UpdateUserDto, UserDto } from "src/modules/user/user.dto";
-import { UserService } from "src/modules/user/user.service";
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { Public } from 'src/config/publicRoutes.decorator';
+import { User } from 'src/entity/user.entity';
+import { UpdateUserDto, UserDto } from 'src/modules/user/user.dto';
+import { UserService } from 'src/modules/user/user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) { }
+  constructor(private readonly userService: UserService) {}
 
   @Public()
   @Post('register')
@@ -18,9 +26,8 @@ export class UserController {
   @Patch(':id')
   async updateUser(
     @Param('id') userId: string,
-    @Body() updateUserDto: UpdateUserDto
+    @Body() updateUserDto: UpdateUserDto,
   ): Promise<User> {
     return await this.userService.update(userId, updateUserDto);
   }
-
 }
